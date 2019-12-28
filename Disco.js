5ec6aa98352a7d4220497d26f2972be9f6957275
THEGUY <38544468+THEGUY3ds@users.noreply.github.com>
2019-12-28 21:09:03 +1100

    Test discord overlay

---
+++ Disco.js
@@ -0,0 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
+  new Crate({
+    server: '643949697119289365',
+    channel: '660372051755794442',
+    shard: 'https://disweb.dashflo.net'
+  })
+</script>

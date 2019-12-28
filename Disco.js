6de6bf1e233e7290de23811ccc7056622d23363a
THEGUY <38544468+THEGUY3ds@users.noreply.github.com>
2019-12-28 21:10:46 +1100

    Update Disco.js

--- Disco.js
+++ Disco.js
@@ -1,7 +1,6 @@
-<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
+src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" 
   new Crate({
     server: '643949697119289365',
     channel: '660372051755794442',
     shard: 'https://disweb.dashflo.net'
   })
-</script>
